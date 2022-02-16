8dc65c5eb53b5bcc02a95ff3e4b57d42c3ce40a0
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:16:08 -0500

    whyred: move super_dummy build to Android.mk
    Change-Id: I035ac0def811c0de2d875121ec6b0c0e44ff25ed
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,19 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),whyred)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+LPFLASH := $(HOST_OUT_EXECUTABLES)/lpflash$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_SUPERIMAGE_DUMMY_TARGET := $(PRODUCT_OUT)/super_dummy.img
+
+$(INSTALLED_SUPERIMAGE_DUMMY_TARGET): $(PRODUCT_OUT)/super_empty.img $(LPFLASH)
+	$(call pretty,"Target dummy super image: $@")
+	$(hide) touch $@
+	$(hide) echo $(CURDIR)
+	$(hide) $(LPFLASH) $@ $(PRODUCT_OUT)/super_empty.img
+
+.PHONY: super_dummyimage
+super_dummyimage: $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
 endif
